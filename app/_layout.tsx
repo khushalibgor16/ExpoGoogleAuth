71ee9fa4079fd1e3e9564451537a27a3adb9923a
khushalibgor16 <khushali@kanhasoft.com>
2026-03-11 10:27:35 +0530

    Update the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import { AuthProviderWithGoogle } from "../src/auth/AuthProviderWithGoogle";
 
-
 export default function RootLayout() {
   return (
     <AuthProviderWithGoogle>
